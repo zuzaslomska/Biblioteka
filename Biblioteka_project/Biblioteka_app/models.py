9c95f158ac34abf5e9a38f07cd7b867a507487f6
zuzaslomska <zuzaslomska@gmail.com>
2019-10-09 18:09:06 +0200

    Create models

--- Biblioteka_project/Biblioteka_app/models.py
+++ Biblioteka_project/Biblioteka_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import URLValidator
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.TextField()
+    author = ArrayField(ArrayField(models.TextField()))
+    published_date = models.DateField()
+    page_count = models.IntegerField()
+    image_links = models.ForeignKey('ImageLinks',on_delete=models.CASCADE)
+    language = models.CharField(max_length=10)
+
+
+class IndustryIdentifiers(models.Model):
+    type = models.TextField()
+    identifier = models.TextField(unique=True)
+    book = models.ForeignKey('Book',on_delete=models.CASCADE)
+
+
+class ImageLinks(models.Model):
+    small_thumbnail = models.TextField(validators=[URLValidator()],blank=True)
+    thumbnail = models.TextField(validators=[URLValidator()],blank=True)
+    small = models.TextField(validators=[URLValidator()],blank=True)
+    medium = models.TextField(validators=[URLValidator()],blank=True)
+    large = models.TextField(validators=[URLValidator()],blank=True)
+    extraLarge = models.TextField(validators=[URLValidator()],blank=True)
